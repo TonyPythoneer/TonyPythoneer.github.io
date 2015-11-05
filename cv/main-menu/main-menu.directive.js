3d34fbca248d0d4cda0f2961cca65da73e7923a5
TonyPythoneer <tonypythoneer@gmail.com>
2015-11-05 21:43:04 +0800

    Enhance: Complete main-menu

--- cv/main-menu/main-menu.directive.js
+++ cv/main-menu/main-menu.directive.js
@@ -5,6 +5,8 @@
         .module('cv')
         .directive('mainMenu', mainMenu);
 
+    ////
+
     function mainMenu() {
         var directive = {
             restrict: 'E',
@@ -20,11 +22,43 @@
     function MainMenuConteroller() {
         var self = this;
         self.menuItems = [
-            {href: "#*"},
-            {href: "#profile-tab"},
-            {href: "#resume-tab"},
-            {href: "#portfolio-tab"}
+            {href: "#*",
+             iconType: "menu",
+             color: "black"},
+            {href: "#profile-tab",
+             iconType: "profile",
+             color: "red"},
+            {href: "#resume-tab",
+             iconType: "resume",
+             color: "keppel"},
+            {href: "#portfolio-tab",
+             iconType: "portfolio",
+             color: "yellow"},
+            {href: "#contact-tab",
+             iconType: "contact",
+             color: "blue"}
         ];
-    };
+        self.getMenuItemIcon = getMenuItemIcon;
+        self.getMenuItemColor = getMenuItemColor;
 
+        ////
+
+        function getMenuItemIcon(iconType){
+            var class_ = {
+                icon: true,
+                sprite: true
+            };
+            class_[iconType] = true;
+            return class_;
+        };
+
+        function getMenuItemColor(color){
+            var class_ = {
+                "menu-item": true,
+            };
+            class_["menu-" + color] = true;
+            return class_;
+        };
+
+    };
 })();
